0c74aae3b0b740ca2584c8e08ed1624b3426eab5
neha <nehaagrawal30@gmail.com>
2017-08-05 17:25:26 -0700

    adding ag-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import {GridOptions} from "ag-grid/main";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Sample CRUD App';
+  private myColumnDefs: any[];
+  private myGridOptions: GridOptions;
+  private rowData;
+
+ngOnInit(): void {
+  this.myGridOptions = this.createGridOptions();
+  this.myColumnDefs = [
+              {headerName: "Make", field: "make"},
+              {headerName: "Model", field: "model"},
+              {headerName: "Price", field: "price"},
+              {headerName: "Year", field: "year"},
+              {headerName: "Color", field: "color"},
+              {headerName: "Rating", field: "rating"},
+              {headerName: "Dealer Name", field: "dealer"},
+          ];
+
+          this.rowData = [
+              {make: "Toyota", model: "Celica", price: 35000 , year: 2017, color : "red", rating : "5", dealer: "Dealer 1"},
+              {make: "Ford", model: "Mondeo", price: 32000, year : 2016 , color : "black" , rating : "4" , dealer : "Dealer 2"},
+              {make: "Porsche", model: "Boxter", price: 72000, year: 2014, color: "grey" , rating : "4" , dealer : "Dealer 3"}
+          ]
+
+}
+
+private createGridOptions ()
+{
+  let gridOptions : GridOptions = {
+   enableColResize: true,
+   enableSorting : true,
+   enableFilter: true,
+   pagination: true
+  };
+
+return gridOptions;
+}
+
+getGridOptions() : GridOptions
+{
+  return this.myGridOptions;
+}
+
+getColumnDef() : any[] {
+  return this.myColumnDefs;
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { TabsModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import {AgGridModule} from "ag-grid-angular/main";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabsModule.forRoot(),
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
